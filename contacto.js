9b4b09fd6f5f1645a4f321a5e54f049db09238fc
val3np <vperugini.21@gmail.com>
2022-09-26 18:59:02 -0300

    otro cambio

---
+++ contacto.js
@@ -0,0 +1,5 @@
+
+function dos() {
+    document.getElementById("uno").style.display="none";
+    document.getElementById("tres").style.display="none";
+}
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const nav = document.getElementById('nav')
-const indicador = document.getElementById('indicador')
-const secciones = document.getElementById('.seccion')
+function accion(){
+    console.log('Esta fucionando mi botón');
+    var ancla = document.getElementsByClassName('nav_ul');
+    for(var i = 0; i < ancla.length; i++){
+        ancla[i].classList.toggle('ocultar');
+    }}
+
+function acciones(){
+    console.log('Esta fucionando mi botón');
+    var enlace = document.getElementsByClassName('nav_ul');
+    for(var i = 0; i < enlace.length; i++){
+        enlace[i].classList.toggle('ocultar');
+    }}
