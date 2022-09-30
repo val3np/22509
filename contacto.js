18a7fb89721ea1e6f2d7b7ec2245edd1f61993b3
val3np <vperugini.21@gmail.com>
2022-09-30 12:08:42 -0300

    mejoras en JS y el nav(sombras)

--- contacto.js
+++ contacto.js
@@ -15,12 +15,14 @@ function acciones(){
         enlace[i].classList.toggle('ocultar');
     }}
 
-//Formulario
 
-const form = document.querySelector('#form')
-form.addEventListener('submit', btnSubmit)
+//Validación del formulario
+
+const form = document.querySelector('#form');
+form.addEventListener('submit', btnSubmit);
+
     async function btnSubmit(e){
-        e.preventDefault(e)
+        e.preventDefault()
         const form = new FormData(this);
         const response = await fetch(this.action , {
             method: this.method,
@@ -30,20 +32,61 @@ form.addEventListener('submit', btnSubmit)
             }
         })
 
+        let nombre = document.getElementById('nameCliente').value;
+        let apellido = document.getElementById('lastName').value;
+        let email = document.getElementById('emailCliente').value;
+        let pais = document.getElementById('pais').value;
+        let textConsulta = document.getElementById('txt-area').value;
+        let elementoActivo = document.querySelector('input[name="motivo"]:checked');
+
+        if (nombre.length == 0 || nombre.length === ''){
+            alert("Debe ingresar su nombre");
+            return;
+        }
+
+        if (apellido.length == 0 || nombre.length === ''){
+            alert("Debe ingresar su apellido");
+            return;
+        }
+
+        if (email.length == 0 || nombre.length === ''){
+            alert("Debe ingresar su email");
+            return;
+        }
+
+
+        if (pais.length == 0 || nombre.length === ''){
+            alert("Debe ingresar su país");
+            return;
+        }
+          
+        if(elementoActivo) {
+        } else {
+            alert('Debe seleccionar un motivo');
+        }
+
+        if (textConsulta.length == 0 || nombre.length === ''){
+            alert("Debe ingresar su consulta o sugerencia");
+            return;
+        }
+   
         if (response.ok){
             this.reset();
             alert('Gracias contactarnos, pronto tendrás una respuesta');
-        }
-}
+            
+    } 
+};
+
+
 
 // Selección de los mapas
 
-const card3 = document.getElementById("card-3")
-const card2 = document.getElementById("card-2")
-const card1 = document.getElementById("card-1")
-const mapa1 = document.getElementById("mapa_1")
-const mapa2 = document.getElementById("mapa_2")
-const mapa3 = document.getElementById("mapa_3")
+const card3 = document.getElementById("card-3");
+const card2 = document.getElementById("card-2");
+const card1 = document.getElementById("card-1");
+const mapa1 = document.getElementById("mapa_1");
+const mapa2 = document.getElementById("mapa_2");
+const mapa3 = document.getElementById("mapa_3");
 
 card1.addEventListener("click", primerMapa)
 card2.addEventListener("click", segundoMapa)
