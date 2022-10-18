4748f58c709ea40d0c8f152c0842a4c23dc6bb3e
Eliseo <eliseorozcow@gmail.com>
2022-10-18 01:16:57 -0300

    Revisando el form

--- index.js
+++ index.js
@@ -1,121 +1,192 @@
 //Para abrir y cerrar el menú
 
-localStorage.setItem("monedero",0)
-localStorage.setItem("cantLibro1",0)
-localStorage.setItem("cantLibro2",0)
-localStorage.setItem("cantLibro3",0)
-localStorage.setItem("cantLibro4",0)
-localStorage.setItem("cantLibro5",0)
-localStorage.setItem("cantLibro6",0)
-localStorage.setItem("cantLibro7",0)
-localStorage.setItem("cantLibro8",0)
-localStorage.setItem("cantLibro9",0)
-localStorage.setItem("cantLibro10",0)
-localStorage.setItem("cantLibro11",0)
-localStorage.setItem("cantLibro12",0)
-localStorage.setItem("cantLibro13",0)
-localStorage.setItem("cantLibro14",0)
-localStorage.setItem("cantLibro15",0)
-localStorage.setItem("cantLibro16",0)
-localStorage.setItem("cantLibro17",0)
-localStorage.setItem("cantLibro18",0)
+localStorage.setItem("monedero", 0)
+localStorage.setItem("cantLibro1", 0)
+localStorage.setItem("cantLibro2", 0)
+localStorage.setItem("cantLibro3", 0)
+localStorage.setItem("cantLibro4", 0)
+localStorage.setItem("cantLibro5", 0)
+localStorage.setItem("cantLibro6", 0)
+localStorage.setItem("cantLibro7", 0)
+localStorage.setItem("cantLibro8", 0)
+localStorage.setItem("cantLibro9", 0)
+localStorage.setItem("cantLibro10", 0)
+localStorage.setItem("cantLibro11", 0)
+localStorage.setItem("cantLibro12", 0)
+localStorage.setItem("cantLibro13", 0)
+localStorage.setItem("cantLibro14", 0)
+localStorage.setItem("cantLibro15", 0)
+localStorage.setItem("cantLibro16", 0)
+localStorage.setItem("cantLibro17", 0)
+localStorage.setItem("cantLibro18", 0)
 
-function accion(){
+function accion() {
     let prueba = document.getElementsByClassName("btn-menu");
-    for(let i = 0; i < prueba.length; i++){
+    for (let i = 0; i < prueba.length; i++) {
         prueba[i].classList.toggle('ocultar');
     }
     let ancla = document.getElementsByClassName('nav_ul');
-    for(let i = 0; i < ancla.length; i++){
+    for (let i = 0; i < ancla.length; i++) {
         ancla[i].classList.toggle('ocultar');
     }
 
 }
 
-function acciones(){
+function acciones() {
     let enlace = document.getElementsByClassName('nav_ul');
-    for(let i = 0; i < enlace.length; i++){
+    for (let i = 0; i < enlace.length; i++) {
         enlace[i].classList.toggle('ocultar');
     }
     let prueba = document.getElementsByClassName("btn-menu");
-    for(let i = 0; i < prueba.length; i++){
+    for (let i = 0; i < prueba.length; i++) {
         prueba[i].classList.toggle('ocultar');
     }
 }
 
 //Validación del formulario
 
+function validarEmail(valor) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(valor)){
+     return true;
+    } else {
+     return false;
+    }
+}
+
 let form = document.querySelector('#form');
 form.addEventListener('submit', btnSubmit);
 
-    async function btnSubmit(e){
-        e.preventDefault()
-        let form = new FormData(this);
-        let response = await fetch(this.action , {
-            method: this.method,
-            body: form,
-            headers: {
-                'Accept':'application/json'
-            }
-        })
-
-        let nombre = document.getElementById('nameCliente').value;
-        let apellido = document.getElementById('lastName').value;
-        let email = document.getElementById('emailCliente').value;
-        let pais = document.getElementById('pais').value;
-        let textConsulta = document.getElementById('txt-area').value;
-        let elementoActivo = document.querySelector('input[name="motivo"]:checked');
-
-        if (nombre.length == 0 || nombre.length === ''){
-            alert("Debe ingresar su nombre");
-            return;
+async function btnSubmit(e) {
+    e.preventDefault()
+    let form = new FormData(this);
+    let response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
         }
+    })
 
-        if (apellido.length == 0 || nombre.length === ''){
-            alert("Debe ingresar su apellido");
-            return;
-        }
+    let nombre = document.getElementById('nameCliente').value;
+    let apellido = document.getElementById('lastName').value;
+    let email = document.getElementById('emailCliente').value;
+    let pais = document.getElementById('pais').value;
+    let textConsulta = document.getElementById('txt-area').value;
+    let elementoActivo = document.querySelector('input[name="motivo"]:checked');
 
-        if (email.length == 0 || nombre.length === ''){
-            alert("Debe ingresar su email");
-            return;
-        }
+    if (nombre.length == 0 || nombre.length === '') {
+        Toastify({
+            text: "Debe ingresar su nombre",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+        return;
+    }
 
-        if (pais.length == 0 || nombre.length === ''){
-            alert("Debe ingresar su país");
-            return;
-        }
-          
-        if(elementoActivo) {
-        } else {
-            alert('Debe seleccionar un motivo');
-        }
+    if (apellido.length == 0 || nombre.length === '') {
+        Toastify({
+            text: "Debe ingresar su apellido",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+        return;
+    }
 
-        if (textConsulta.length == 0 || nombre.length === ''){
-            alert("Debe ingresar su consulta o sugerencia");
-            return;
-        }
-   
-        if (response.ok){
-            this.reset();
-            Toastify({
-                text: "Gracias contactarnos, pronto tendrás una respuesta",
-                duration: 3000,
-                destination: "index.html",
-                newWindow: true,
-                gravity: "top", 
-                position: "right",
-                stopOnFocus: true,
-                style: {
-                  background: "linear-gradient(to right, #E9EBEE, #4267B3)",
-                },
-                onClick: function(){} // Callback after click
-              }).showToast();
-            
-    } 
+    if (email.length == 0 || email.length === '') {
+        Toastify({
+            text: "Debe ingresar su email",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+        return;
+    }
+
+    if (pais.length == 0 || nombre.length === '') {
+        Toastify({
+            text: "Debe ingresar su pais",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+        return;
+    }
+
+    if (elementoActivo) {
+    } else {
+        Toastify({
+            text: "Seleccione su motivo por favor",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();;
+    }
+
+    if (textConsulta.length == 0 || nombre.length === '') {
+        Toastify({
+            text: "Debe ingresar su sugerencia",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+        return;
+    }
+
+    if (response.ok) {
+        this.reset();
+        Toastify({
+            text: "Gracias contactarnos, pronto tendrás una respuesta",
+            duration: 3000,
+            destination: "index.html",
+            newWindow: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #E9EBEE, #4267B3)",
+            },
+        }).showToast();
+
+    }
 };
-
-
 
 // Selección de los mapas
 
@@ -130,19 +201,19 @@ card1.addEventListener("click", primerMapa);
 card2.addEventListener("click", segundoMapa);
 card3.addEventListener("click", tercerMapa);
 
-function tercerMapa(){
+function tercerMapa() {
     mapa3.style.display = "flex";
     mapa1.style.display = "none";
     mapa2.style.display = "none";
 }
 
-function primerMapa(){
+function primerMapa() {
     mapa1.style.display = "flex";
     mapa3.style.display = "none";
     mapa2.style.display = "none";
 }
 
-function segundoMapa(){
+function segundoMapa() {
     mapa2.style.display = "flex";
     mapa3.style.display = "none";
     mapa1.style.display = "none";
@@ -150,7 +221,7 @@ function segundoMapa(){
 
 //Seleccionar una card
 
-function verInfoCard(){
+function verInfoCard() {
     document.getElementById("libros").style.display = "flex";
     document.getElementById("libro1").style.display = "flex";
     document.getElementById("libro2").style.display = "none";
@@ -160,7 +231,7 @@ function verInfoCard(){
     document.getElementById("triangulo3").style.display = "none";
 };
 
-function verInfoCard2(){
+function verInfoCard2() {
     document.getElementById("libros").style.display = "flex";
     document.getElementById("libro1").style.display = "none";
     document.getElementById("libro2").style.display = "flex";
@@ -170,7 +241,7 @@ function verInfoCard2(){
     document.getElementById("triangulo3").style.display = "none";
 };
 
-function verInfoCard3(){
+function verInfoCard3() {
     document.getElementById("libros").style.display = "flex";
     document.getElementById("libro1").style.display = "none";
     document.getElementById("libro2").style.display = "none";
@@ -180,7 +251,7 @@ function verInfoCard3(){
     document.getElementById("triangulo1").style.display = "none";
 };
 
-function verInfoCard4(){
+function verInfoCard4() {
     document.getElementById("libros2").style.display = "flex";
     document.getElementById("libro4").style.display = "flex";
     document.getElementById("libro5").style.display = "none";
@@ -190,7 +261,7 @@ function verInfoCard4(){
     document.getElementById("triangulo6").style.display = "none";
 };
 
-function verInfoCard5(){
+function verInfoCard5() {
     document.getElementById("libros2").style.display = "flex";
     document.getElementById("libro4").style.display = "none";
     document.getElementById("libro5").style.display = "flex";
@@ -200,7 +271,7 @@ function verInfoCard5(){
     document.getElementById("triangulo4").style.display = "none";
 };
 
-function verInfoCard6(){
+function verInfoCard6() {
     document.getElementById("libros2").style.display = "flex";
     document.getElementById("libro4").style.display = "none";
     document.getElementById("libro5").style.display = "none";
@@ -210,7 +281,7 @@ function verInfoCard6(){
     document.getElementById("triangulo4").style.display = "none";
 };
 
-function verInfoCard7(){
+function verInfoCard7() {
     document.getElementById("libros3").style.display = "flex";
     document.getElementById("libro7").style.display = "flex";
     document.getElementById("libro8").style.display = "none";
@@ -220,7 +291,7 @@ function verInfoCard7(){
     document.getElementById("triangulo9").style.display = "none";
 };
 
-function verInfoCard8(){
+function verInfoCard8() {
     document.getElementById("libros3").style.display = "flex";
     document.getElementById("libro7").style.display = "none";
     document.getElementById("libro8").style.display = "flex";
@@ -230,7 +301,7 @@ function verInfoCard8(){
     document.getElementById("triangulo9").style.display = "none";
 };
 
-function verInfoCard9(){
+function verInfoCard9() {
     document.getElementById("libros3").style.display = "flex";
     document.getElementById("libro7").style.display = "none";
     document.getElementById("libro8").style.display = "none";
@@ -240,7 +311,7 @@ function verInfoCard9(){
     document.getElementById("triangulo7").style.display = "none";
 };
 
-function verInfoCard10(){
+function verInfoCard10() {
     document.getElementById("libros4").style.display = "flex";
     document.getElementById("libro10").style.display = "flex";
     document.getElementById("libro11").style.display = "none";
@@ -250,7 +321,7 @@ function verInfoCard10(){
     document.getElementById("triangulo12").style.display = "none";
 };
 
-function verInfoCard11(){
+function verInfoCard11() {
     document.getElementById("libros4").style.display = "flex";
     document.getElementById("libro10").style.display = "none";
     document.getElementById("libro11").style.display = "flex";
@@ -260,7 +331,7 @@ function verInfoCard11(){
     document.getElementById("triangulo12").style.display = "none";
 };
 
-function verInfoCard12(){
+function verInfoCard12() {
     document.getElementById("libros4").style.display = "flex";
     document.getElementById("libro10").style.display = "none";
     document.getElementById("libro11").style.display = "none";
@@ -270,7 +341,7 @@ function verInfoCard12(){
     document.getElementById("triangulo10").style.display = "none";
 };
 
-function verInfoCard13(){
+function verInfoCard13() {
     document.getElementById("libros5").style.display = "flex";
     document.getElementById("libro13").style.display = "flex";
     document.getElementById("libro14").style.display = "none";
@@ -280,7 +351,7 @@ function verInfoCard13(){
     document.getElementById("triangulo15").style.display = "none";
 };
 
-function verInfoCard14(){
+function verInfoCard14() {
     document.getElementById("libros5").style.display = "flex";
     document.getElementById("libro13").style.display = "none";
     document.getElementById("libro14").style.display = "flex";
@@ -290,7 +361,7 @@ function verInfoCard14(){
     document.getElementById("triangulo15").style.display = "none";
 };
 
-function verInfoCard15(){
+function verInfoCard15() {
     document.getElementById("libros5").style.display = "flex";
     document.getElementById("libro13").style.display = "none";
     document.getElementById("libro14").style.display = "none";
@@ -300,7 +371,7 @@ function verInfoCard15(){
     document.getElementById("triangulo14").style.display = "none";
 };
 
-function verInfoCard16(){
+function verInfoCard16() {
     document.getElementById("libros6").style.display = "flex";
     document.getElementById("libro16").style.display = "flex";
     document.getElementById("libro17").style.display = "none";
@@ -310,7 +381,7 @@ function verInfoCard16(){
     document.getElementById("triangulo18").style.display = "none";
 };
 
-function verInfoCard17(){
+function verInfoCard17() {
     document.getElementById("libros6").style.display = "flex";
     document.getElementById("libro16").style.display = "none";
     document.getElementById("libro17").style.display = "flex";
@@ -320,7 +391,7 @@ function verInfoCard17(){
     document.getElementById("triangulo18").style.display = "none";
 };
 
-function verInfoCard18(){
+function verInfoCard18() {
     document.getElementById("libros6").style.display = "flex";
     document.getElementById("libro16").style.display = "none";
     document.getElementById("libro17").style.display = "none";
@@ -331,7 +402,7 @@ function verInfoCard18(){
 };
 
 
-function cerrar(){
+function cerrar() {
     document.getElementById("libros").style.display = "none";
     document.getElementById("triangulo1").style.display = "none";
     document.getElementById("triangulo2").style.display = "none";
@@ -358,54 +429,54 @@ function cerrar(){
     document.getElementById("triangulo18").style.display = "none";
 };
 
-function compraLibro(){
+function compraLibro() {
     Toastify({
         text: "Has comprado este libro!",
         duration: 3000,
         destination: "https://facebook.com",
         newWindow: true,
         close: true,
-        gravity: "top", 
+        gravity: "top",
         position: "right",
         stopOnFocus: true,
         style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
         },
-      }).showToast();
+    }).showToast();
 }
 
-function suma_libro(num,id){
+function suma_libro(num, id) {
     let suma = parseInt(num)
-    let nombreId = "cantLibro"+ id
+    let nombreId = "cantLibro" + id
     let nuevaCant = document.getElementById(nombreId)
     nuevaCant.innerText = parseInt(nuevaCant.innerText) + 1
-    localStorage.setItem(nombreId,localStorage.getItem(nombreId)+1)
-    localStorage.setItem("monedero",parseInt(parseInt(localStorage.getItem("monedero")) + suma))
+    localStorage.setItem(nombreId, localStorage.getItem(nombreId) + 1)
+    localStorage.setItem("monedero", parseInt(parseInt(localStorage.getItem("monedero")) + suma))
     Toastify({
-        text: "Producto agregado al carrito!" + localStorage.getItem("monedero"),
+        text: "Producto agregado al carrito!",
         duration: 3000,
         gravity: 'up',
         position: 'right',
     }).showToast();
 }
 
-function quita_libro(num,id){
+function quita_libro(num, id) {
     let resta = parseInt(num)
-    let nombreId = "cantLibro"+ id
+    let nombreId = "cantLibro" + id
     let nuevaCant = document.getElementById(nombreId)
-    if(0 == parseInt(nuevaCant.innerText))
+    if (0 == parseInt(nuevaCant.innerText))
         Toastify({
             text: "No puedes hacer eso, agrega productos primero",
             duration: 3000,
             gravity: 'up',
             position: 'right',
         }).showToast();
-    else{
+    else {
         nuevaCant.innerText = parseInt(nuevaCant.innerText) - 1
-        localStorage.setItem(nombreId,localStorage.getItem(nombreId) - 1)
-        localStorage.setItem("monedero",parseInt(parseInt(localStorage.getItem("monedero")) - resta))
+        localStorage.setItem(nombreId, localStorage.getItem(nombreId) - 1)
+        localStorage.setItem("monedero", parseInt(parseInt(localStorage.getItem("monedero")) - resta))
         Toastify({
-            text: "Producto removido con exito" + localStorage.getItem("monedero"),
+            text: "Producto removido con exito",
             duration: 3000,
             gravity: 'up',
             position: 'right',
